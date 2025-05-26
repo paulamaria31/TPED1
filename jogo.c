63e307c30220c81676d65ae651858b206cbf6635
root <mariapaulamirandagasparini@gmail.com>
2025-05-26 09:29:21 -0300

    Fazendo a alocação dinamica de tabuleiros e dando free neles

--- jogo.c
+++ jogo.c
@@ -2,5 +2,14 @@
 #include "tp.h"
 
 int main() {
+    Tabuleiro* tabuleiros;
+    int n;
+    //Lendo a quantidade de tabuleiros
+    scanf("%d", &n);
+    //Fazendo a alocação dinâmica
+    tabuleiros = alocarTabuleiros(n);
+    
+    desalocarTabuleiro(&tabuleiros);
+
     return 0;
 }
